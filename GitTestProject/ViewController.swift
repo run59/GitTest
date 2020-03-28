922cb62ec33dc231c3b59e94786d6415127e3f0b
run59 <asteroid.tr@gmail.com>
2020-03-27 19:07:51 -0700

    commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by ONUR on 27.03.2020.
+//  Copyright © 2020 ONUR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("git")
+        print("wqeqwe")
+    }
+
+
+}
+
